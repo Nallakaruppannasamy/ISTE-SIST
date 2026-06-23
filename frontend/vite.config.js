399d0435a2a4da8e0dc87451e2cafbcda19621ce
Nallakaruppannasamy <rnksamy007@gmail.com>
2026-06-23 21:02:27 +0530

    deployment error issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: 'es2022' // Forces Rollup to support modern JS syntax during build
+  }
 })
